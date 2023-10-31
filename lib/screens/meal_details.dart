68521e9ffad0a65c5eee9a211262f46728da2a9f
Shrimad Bhagwat <51125208+Shrimad-Bhagwat@users.noreply.github.com>
2023-10-31 12:19:09 +0530

    animations

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -2,10 +2,15 @@ import 'package:cook_book/models/meal.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cook_book/providers/favorites_provider.dart';
+
 class MealDetailsScreen extends ConsumerWidget {
-  const MealDetailsScreen({super.key, required this.meal,});
+  const MealDetailsScreen({
+    super.key,
+    required this.meal,
+  });
 
   final Meal meal;
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final favoriteMeals = ref.watch(favoriteMealsProvider);
@@ -14,37 +19,55 @@ class MealDetailsScreen extends ConsumerWidget {
         appBar: AppBar(
           title: Text(meal.title),
           actions: [
-            IconButton(onPressed: (){
-              // onToggleFavorite(meal);
-              final wasAdded = ref.read(favoriteMealsProvider.notifier).toggleMealFavoriteStatus(meal);
-              ScaffoldMessenger.of(context).clearSnackBars();
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  duration: const Duration(seconds: 2),
-                  content: Text(wasAdded ? 'Meal added as a favorite.': 'Meal removed from favorite.'),
-                ),
-              );
-            }, icon:  Icon(isFavorite ? Icons.star : Icons.star_border))
+            IconButton(
+                onPressed: () {
+                  // onToggleFavorite(meal);
+                  final wasAdded = ref
+                      .read(favoriteMealsProvider.notifier)
+                      .toggleMealFavoriteStatus(meal);
+                  ScaffoldMessenger.of(context).clearSnackBars();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      duration: const Duration(seconds: 2),
+                      content: Text(wasAdded
+                          ? 'Meal added as a favorite.'
+                          : 'Meal removed from favorite.'),
+                    ),
+                  );
+                },
+                icon: AnimatedSwitcher(
+                  transitionBuilder: (child, animation){
+                    return RotationTransition(turns: Tween(begin: 0.8, end: 1.0).animate(animation), child: child,);
+                  },
+                  duration: const Duration(milliseconds: 300),
+                  child: Icon(isFavorite ? Icons.star : Icons.star_border, key: ValueKey(isFavorite),),
+                ))
           ],
         ),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              Image.network(
-                meal.imageUrl,
-                width: double.infinity,
-                height: 300,
-                fit: BoxFit.cover,
+              Hero(
+                tag: meal.id,
+                child: Image.network(
+                  meal.imageUrl,
+                  width: double.infinity,
+                  height: 300,
+                  fit: BoxFit.cover,
+                ),
               ),
               const SizedBox(
                 height: 14,
               ),
-              Text('Ingredients',
+              Text(
+                'Ingredients',
                 style: Theme.of(context).textTheme.titleLarge!.copyWith(
                     color: Theme.of(context).colorScheme.primary,
                     fontWeight: FontWeight.bold),
               ),
-              const SizedBox(height: 16,),
+              const SizedBox(
+                height: 16,
+              ),
               for (final ingredient in meal.ingredients)
                 Text(
                   ingredient,
@@ -52,9 +75,9 @@ class MealDetailsScreen extends ConsumerWidget {
                         color: Theme.of(context).colorScheme.onBackground,
                       ),
                 ),
-
-              const SizedBox(height: 24,),
-
+              const SizedBox(
+                height: 24,
+              ),
               Text(
                 'Steps',
                 style: Theme.of(context).textTheme.titleLarge!.copyWith(
@@ -66,7 +89,8 @@ class MealDetailsScreen extends ConsumerWidget {
               ),
               for (final step in meal.steps)
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8),
                   child: Text(
                     step,
                     textAlign: TextAlign.center,
